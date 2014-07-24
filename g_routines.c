2b298b1f0de9c26e592e98eb0450f8df0ab95fd0
Andreas Bülling <andreas@DUPERDATORN>
2014-07-24 16:02:17 +0200

    cleanup in c routines

--- g_routines.c
+++ g_routines.c
@@ -1,41 +1,42 @@
 
 #include <stdio.h>
-#include "SDL/SDL_main.h"
-#include "SDL/SDL.h"
 
-#define PM 4 /* Pixel magnifier */
-#define PMW 0.5 /* Pixel magnifier */
+#include <SDL/SDL_main.h>
+#include <SDL/SDL.h>
 
+static const char k_scale = 4; /* scale constant */
 SDL_Surface *screen;
 
-void g_init(int w, int h){
-  SDL_Init( SDL_INIT_EVERYTHING );
-  screen = SDL_SetVideoMode( w, h, 32, SDL_SWSURFACE );
-
+void g_init(int iw, int ih)
+{
+  SDL_Init(SDL_INIT_EVERYTHING);
   SDL_WM_SetCaption("Assembler snake", "Assembler snake");
+  screen = SDL_SetVideoMode(iw*k_scale, ih*k_scale, 32, SDL_SWSURFACE);
   printf("init graphics\n");
 }
 
-void g_draw(unsigned char *arr, unsigned char len){
+void g_draw(unsigned char *arr, unsigned char len)
+{
   int i;
-  SDL_Rect r;
-  r.w = PM;
-  r.h = PM;
+  SDL_Rect r = {.w = k_scale, .h = k_scale};
 
+  /* draw background */
   SDL_FillRect(screen, NULL, 0x000000);
 
-  for(i = 0; i < len*2; i+=2){
+  /* draw snake */
+  for (i = 0; i < len*2; i+=2)
+  {
     printf("x: %d, y: %d\n", *(arr-i), *(arr-i-1));
-    r.x = *(arr-i) * PM;
-    r.y = *(arr-i-1) * PM;
+    r.x = *(arr-i) * k_scale;
+    r.y = *(arr-i-1) * k_scale;
     SDL_FillRect(screen, &r, 0xffffff);
   }
   
-  // draw apple
+  /* draw apple */
   printf("apple (%d, %d)\n", *(arr - 66), *(arr - 65)); 
-  r.x = *(arr - 66) * PM;
-  r.y = *(arr - 65) * PM;
+  r.x = *(arr - 66) * k_scale;
+  r.y = *(arr - 65) * k_scale;
   SDL_FillRect(screen, &r, 0xff0000);
 
-  SDL_Flip( screen );
+  SDL_Flip(screen);
 }
