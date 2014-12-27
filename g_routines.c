0a3ec0dde0d303ae7b27ea307041dcea0ead6f95
Andreas Bülling <andreas@DUPERDATORN>
2014-12-27 14:14:45 +0100

    start adding score

--- g_routines.c
+++ g_routines.c
@@ -5,13 +5,16 @@
 #include <SDL/SDL.h>
 
 static const char k_scale = 4; /* scale constant */
+static const char k_header_height = 30; /* height of header section*/
 SDL_Surface *screen;
 
 void g_init(int iw, int ih)
 {
   SDL_Init(SDL_INIT_EVERYTHING);
   SDL_WM_SetCaption("Assembler snake", "Assembler snake");
-  screen = SDL_SetVideoMode(iw*k_scale, ih*k_scale, 32, SDL_SWSURFACE);
+  int window_width = iw*k_scale;
+  int window_height = ih*k_scale + k_header_height;
+  screen = SDL_SetVideoMode(window_width, window_height, 32, SDL_SWSURFACE);
   printf("init graphics\n");
 }
 
@@ -23,19 +26,22 @@ void g_draw(unsigned char *arr, unsigned char len)
   /* draw background */
   SDL_FillRect(screen, NULL, 0x000000);
 
+  /* draw title + score */
+  
+
   /* draw snake */
   for (i = 0; i < len*2; i+=2)
   {
     printf("x: %d, y: %d\n", *(arr-i), *(arr-i-1));
     r.x = *(arr-i) * k_scale;
-    r.y = *(arr-i-1) * k_scale;
+    r.y = *(arr-i-1) * k_scale + k_header_height;
     SDL_FillRect(screen, &r, 0xffffff);
   }
   
   /* draw apple */
   printf("apple (%d, %d)\n", *(arr - 66), *(arr - 65)); 
   r.x = *(arr - 66) * k_scale;
-  r.y = *(arr - 65) * k_scale;
+  r.y = *(arr - 65) * k_scale + k_header_height;
   SDL_FillRect(screen, &r, 0xff0000);
 
   SDL_Flip(screen);
